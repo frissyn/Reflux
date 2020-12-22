7c959619a3ccaac5f1ac62930addcb7a866ecb0d
Name?= null <Ire.Raufu76@gmail.com>
2020-12-22 17:57:32 +0000

    add PyPI dist script

---
+++ actions/distribute.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Upgrading Dist Tools..."
+
+pip install --upgrade setuptools wheel twine
+
+clear
+echo "Building Distribution..."
+
+python3 setup.py sdist bdist_wheel
+
+clear
+echo "Executing Distribution..."
+
+python3 -m twine upload dist/*
+
+clear
+echo "Done!"
