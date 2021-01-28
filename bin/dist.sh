14748138a9ae3f0438112f8a54512d3cc993dd96
frissyn <62220201+frissyn@users.noreply.github.com>
2021-01-28 10:10:16 -0500

    update PyPI dist script

--- bin/dist.sh
+++ bin/dist.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 
+function propmt_clear() {
+    echo -n "Clear the screen? >> "
+    read res
+
+    if [ $res == "yes" ]; then
+        clear
+    else
+        echo "Skipping clear screen..."
+    fi
+}
+
 echo "Upgrading Dist Tools..."
-
 pip install --upgrade setuptools wheel twine
+propmt_clear
 
-clear
 echo "Building Distribution..."
-
 python3 setup.py sdist bdist_wheel
+propmt_clear
 
-clear
+
 echo "Executing Distribution..."
-
 python3 -m twine upload dist/*
+propmt_clear
 
 echo "Dist Done!"
 echo "Removing PKG files/folders..."
 
-rm -r "build"
-rm -r "dist"
-rm -r "reflux.egg-info"
+rm -r "build"; rm -r "dist"
+rm -r "kitsune.py.egg-info"
 
 echo "Done!"
