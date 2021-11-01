e688d15d629aeff9b5feb12cc17fdfc6785ebba0
Ire <62220201+frissyn@users.noreply.github.com>
2021-11-01 18:14:44 +0000

    🏷️: release for 1.0.0~~

--- reflux/__init__.py
+++ reflux/__init__.py
@@ -1,9 +1,6 @@
 from .theme import Theme
 
-from .resources import path
-
 __all__ = ["Theme"]
-
 __author__ = "frissyn"
-__version__ = "0.3.0"
-__doc__ = "Python package for creating IDE themes on Repl.it"
+__version__ = "1.0.0"
+__doc__ = "Python package for generating IDE themes on Replit"
