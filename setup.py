37be5d93e3b6eff455f8a8dd9125f9b8fe86cf62
Ire <62220201+frissyn@users.noreply.github.com>
2021-11-02 20:25:38 +0000

    📦: update package data, includes, metadata, etc.

--- setup.py
+++ setup.py
@@ -1,29 +1,30 @@
 import reflux
 import setuptools
 
-url = "https://github.com/frissyn/reflux"
-# readme = open("README.md").read()
-description = "Python package for creating IDE themes on Replit."
+readme = open("README.md").read()
+url = "https://github.com/frissyn/Reflux"
+description = "Extensive styling and theming functionality for Replit."
 
 setuptools.setup(
     name="reflux",
-
     license="MIT",
     description=description,
     version=reflux.__version__,
-    packages=setuptools.find_packages(),
+    packages=["reflux"],
     project_urls={
         "Source Code": url,
         "Pull Requests": url + "/pulls",
         "Issue Tracker": url + "/issues",
+        "Documentation": url + "/tree/master/docs"
     },
 
-    # long_description=readme,
-    # long_description_content_type="text/markdown",
+    long_description=readme,
+    long_description_content_type="text/markdown",
 
+    python_requires=">=3.7.0",
+    install_requires=["pyyaml"],
     include_package_data=True,
-    python_requires=">=3.8.0",
-    install_requires=["pyyaml>=6.0"],
+    package_data={"": ["*.txt", "*.js"]},
 
     classifiers=[
         "License :: OSI Approved :: MIT License",
