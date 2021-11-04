c10f7d1748e8aea7f31092236677895fa7d13221
Ire <62220201+frissyn@users.noreply.github.com>
2021-11-04 16:46:04 +0000

    🏷️: release for 1.0.2...

--- reflux/__init__.py
+++ reflux/__init__.py
@@ -4,5 +4,5 @@ from reflux.resources import shelf as Resources
 
 __all__ = ["Theme", "Resources"]
 __author__ = "frissyn"
-__version__ = "1.0.1"
-__doc__ = "Python package for generating IDE themes on Replit"
+__version__ = "1.0.2"
+__doc__ = "Extensive styling and theming functionality for Replit."
