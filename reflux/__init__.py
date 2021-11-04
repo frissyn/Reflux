9c470d1f8d8b1fa1708a9147813419823269fd18
Ire <62220201+frissyn@users.noreply.github.com>
2021-11-04 17:39:59 +0000

    🏷️: alpha release for 1.0.3

--- reflux/__init__.py
+++ reflux/__init__.py
@@ -4,5 +4,5 @@ from reflux.resources import shelf as Resources
 
 __all__ = ["Theme", "Resources"]
 __author__ = "frissyn"
-__version__ = "1.0.2"
+__version__ = "1.0.3a"
 __doc__ = "Extensive styling and theming functionality for Replit."
