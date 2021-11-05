bafccb80bc45df97e74a1e4a7b156fa92d18ab0a
Ire <62220201+frissyn@users.noreply.github.com>
2021-11-05 16:17:56 +0000

    🏷️: release for 1.1.0 (#7)

--- reflux/__init__.py
+++ reflux/__init__.py
@@ -1,8 +1,9 @@
 from reflux.theme import Theme
+from reflux.theme import make_engine
 
 from reflux.resources import shelf as Resources
 
-__all__ = ["Theme", "Resources"]
+__all__ = ["Theme", "Resources", "make_engine"]
 __author__ = "frissyn"
-__version__ = "1.0.3a1"
+__version__ = "1.1.0"
 __doc__ = "Extensive styling and theming functionality for Replit."
